bdd0d8a8e6d214a24bbc58e287ff59d3b6d90ac6
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-16 13:48:52 -0300

    Replaced deprecated code

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -21,8 +21,12 @@ class ViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        cell.textLabel?.text = "Title goes here"
-        cell.detailTextLabel?.text = "Subtitle goes here"
+        
+        var content = cell.defaultContentConfiguration()
+        content.text = "Title goes here"
+        content.secondaryText = "Subtitle goes here"
+        cell.contentConfiguration = content
+
         return cell
     }
 }
