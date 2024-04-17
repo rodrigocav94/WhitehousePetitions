4c42e1c029b1c81ac81e51b6e5e3beb60ea9d0f6
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-17 16:44:25 -0300

    Added a new tab bar item; Added an alert to be displayed the app was unable to load the url

--- WhitehousePetitions/ViewController.swift
+++ WhitehousePetitions/ViewController.swift
@@ -40,13 +40,21 @@ class ViewController: UITableViewController {
     }
     
     func loadData() {
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+                return
             }
         }
+        showError()
     }
     
     func parse(json: Data) {
@@ -56,6 +64,12 @@ class ViewController: UITableViewController {
             petitions = jsonPetitions.results
             self.tableView.reloadData()
         }
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
 }
 
